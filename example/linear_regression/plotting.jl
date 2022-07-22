344c83b6dd2c242a5942d026f395e6700bdc9707
zuhengxu <zuhengdavid@gmail.com>
2022-07-21 23:47:08 -0700

    minor edit

--- example/linear_regression/plotting.jl
+++ example/linear_regression/plotting.jl
@@ -12,7 +12,8 @@ Ns = ELBO["Ns"]
 Labels = Matrix{String}(undef, 1, size(eps, 1))
 Labels[1, :].= ["ϵ=$e" for e in eps] 
 
-p_elbo = plot(reduce(vcat, [[0], Ns]), Els',lw = 3, labels = Labels, legend = :outertopright, ylabel = "ELBO", xlabel = "#Refreshment", title = "Bayesian Linear Regression", xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm)
+p_elbo = plot(reduce(vcat, [[0], Ns]), Els',lw = 3, labels = Labels, legend = :outertopright, ylabel = "ELBO", xlabel = "#Refreshment", title = "Bayesian Linear Regression", 
+                        xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm)
 savefig(p_elbo, "figure/linreg_elbo.png")
 
 # KSD
@@ -25,10 +26,12 @@ nBs = KSD["nBurns"]
 
 Labels = Matrix{String}(undef, 1, size(nBs, 1))
 Labels[1, :].= ["ErgFlow"] 
-p_ksd = plot(reduce(vcat, [[0], Ns]), Ks',lw = 3, ylim = (0, Inf),labels = Labels, ylabel = "Marginal KSD", xlabel = "#Refreshment", title  = "Bayesian Linear Regression", xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm)
+p_ksd = plot(reduce(vcat, [[0], Ns]), Ks',lw = 3, ylim = (0, Inf),labels = Labels, ylabel = "Marginal KSD", xlabel = "#Refreshment", title  = "Bayesian Linear Regression", 
+            xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm)
 hline!([ksd_nuts], linestyle=:dash, lw = 2, label = "NUTS")
 savefig(p_ksd, "figure/linreg_ksd.png")
 
-p_ksd = plot(reduce(vcat, [[0], Ns]), Ks',lw = 3, ylim = (1., 1000.),labels = Labels, ylabel = "Marginal KSD", xlabel = "#Refreshment", title  = "Bayesian Linear Regression", xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm, yaxis=:log)
+p_ksd = plot(reduce(vcat, [[0], Ns]), Ks',lw = 3, ylim = (1., 1000.),labels = Labels, ylabel = "Marginal KSD", xlabel = "#Refreshment", title  = "Bayesian Linear Regression", 
+            xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm, yaxis=:log)
 hline!([ksd_nuts], linestyle=:dash, lw = 2, label = "NUTS")
 savefig(p_ksd, "figure/linreg_ksd_log.png")
