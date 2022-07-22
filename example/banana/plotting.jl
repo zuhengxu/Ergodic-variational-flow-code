571203903eca9b7451a7b7f8eede981f7a808f95
zuhengxu <zuhengdavid@gmail.com>
2022-07-21 23:12:08 -0700

    clean banana

--- example/banana/plotting.jl
+++ example/banana/plotting.jl
@@ -36,3 +36,18 @@ Labels[1, :].= ["ErgFlow"]
 p_ksd = plot(reduce(vcat, [[0], Ns]), Ks',lw = 3, ylim = (0, Inf),labels = Labels, ylabel = "Marginal KSD", xlabel = "#Refreshment", title  = "Banana", xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 20, margin=5Plots.mm)
 hline!( [ksd_nuts], linestyle=:dash, lw = 2, label = "NUTS")
 savefig(p_ksd, "figure/banana_ksd.png")
+
+# stability plot
+lap = JLD.load("result/stab_lap.jld")
+gauss = JLD.load("result/stab_norm.jld")
+logis =  JLD.load("result/stab_log.jld")
+
+plot(lap["Ns"], vec(median(lap["fwd_err"], dims=2)), ribbon = get_percentiles(lap["fwd_err"]), lw = 3, label = "Lap Fwd", xlabel = "#refreshments", ylabel = "error", title = "Banana", 
+    xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 45, legend=:outertopright) 
+plot!(lap["Ns"], vec(median(lap["bwd_err"], dims=2)), ribbon = get_percentiles(lap["bwd_err"]), lw = 3, label = "Lap Bwd") 
+plot!(lap["Ns"], vec(median(logis["fwd_err"], dims=2)), ribbon = get_percentiles(logis["fwd_err"]), lw = 3, label = "logis Fwd", xlabel = "#refreshments", ylabel = "error", title = "Banana", 
+    xtickfontsize=18, ytickfontsize=18, guidefontsize=18, legendfontsize=18, titlefontsize = 18, xrotation = 45, legend = :outertopright) 
+plot!(lap["Ns"], vec(median(logis["bwd_err"], dims=2)), ribbon = get_percentiles(logis["bwd_err"]), lw = 3, label = "logis Bwd") 
+plot!(lap["Ns"], vec(median(gauss["fwd_err"], dims=2)), ribbon = get_percentiles(gauss["fwd_err"]), lw = 3, label = "Gauss Fwd") 
+plot!(lap["Ns"], vec(median(gauss["bwd_err"], dims=2)), ribbon = get_percentiles(gauss["bwd_err"]), lw = 3, label = "Gauss Bwd") 
+savefig("figure/banana_stability.png")
