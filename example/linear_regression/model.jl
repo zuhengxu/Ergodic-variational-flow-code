c80365a5920064f25cfabd0da22098b5eb33d29e
NaitongChen <chennaitong218@hotmail.com>
2023-05-09 12:31:20 -0700

    string type issue

--- example/linear_regression/model.jl
+++ example/linear_regression/model.jl
@@ -23,7 +23,7 @@ using DataFrames, CSV
 # xs = hcat(X, Y)
 
 cd("/arc/project/st-tdjc-1/mixflow/Ergodic-variational-flow-code/example/linear_regression")
-xs = Float64.(Matrix(CSV.read("data/Dat.csv", DataFrame)))
+xs = Matrix{Float64}(CSV.read("data/Dat.csv", DataFrame, stringtype=String))
 N = size(xs, 1)
 d = size(xs, 2) + 1 # 13 features plus intercept and error variance
 fs = Float64.(hcat(ones(N), xs[:,1:d-2]))
